c446177a0cf4bbe7798c03ab3a559e88b497cfdb
One-LifeMan <fomenko.anton.biz@gmail.com>
2024-04-09 16:08:26 +0300

    added variability in the choice of font generation

--- config/gulp-tasks/fonts.js
+++ config/gulp-tasks/fonts.js
@@ -4,34 +4,40 @@ import ttf2woff from "gulp-ttf2woff";
 import ttf2woff2 from "gulp-ttf2woff2";
 import environments from "gulp-environments";
 
+const convertTTFToWOFF = false;
+
 const { development, production } = environments;
 
-//  OLD
-// const SOURCE = ["./src/fonts/**/*.ttf"];
-// NEW
-const SOURCE = ["./src/fonts/**/*"];
 
 let destination = development() ? "dev/fonts" : "dist/fonts";
 
-// OLD
-// function fonts() {
-//     return gulp
-//         .src(SOURCE)
-//         .pipe(changed(destination))
-//         .pipe(ttf2woff())
-//         .pipe(gulp.dest(destination))
-
-//         .pipe(gulp.src(SOURCE))
-//         .pipe(ttf2woff2())
-//         .pipe(gulp.dest(destination))
-
-//         .pipe(gulp.src("./src/fonts/**/*"))
-//         .pipe(gulp.dest(destination));
-// }
-
-// NEW
 function fonts() {
-    return gulp.src(SOURCE).pipe(changed(destination)).pipe(gulp.dest(destination));
+    if(convertTTFToWOFF) {
+        const SOURCE = ["./src/fonts/**/*.ttf"];
+
+        return gulp
+            .src(SOURCE)
+            .pipe(changed(destination))
+            .pipe(ttf2woff())
+            .pipe(gulp.dest(destination))
+
+            .pipe(gulp.src(SOURCE))
+            .pipe(ttf2woff2())
+            .pipe(gulp.dest(destination))
+
+            .pipe(gulp.src("./src/fonts/**/*"))
+            .pipe(gulp.dest(destination));
+
+
+
+    } else {
+        const SOURCE = ["./src/fonts/**/*"];
+
+        return gulp
+            .src(SOURCE)
+            .pipe(changed(destination))
+            .pipe(gulp.dest(destination));
+    }
 }
 
 export { fonts };
